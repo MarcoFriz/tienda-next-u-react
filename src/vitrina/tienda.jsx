9f9fd124c5793b3a69e5944573cf741ceeaf57b4
MarcoFriz <marco.friz.p@gmail.com>
2019-01-05 19:21:25 -0300

    7° mostrar la lista de porductos en el carrito: ok

--- src/vitrina/tienda.jsx
+++ src/vitrina/tienda.jsx
@@ -29,40 +29,37 @@ class Tienda extends React.Component {
 	render() {
 		const total = this.state.total;
 		const list = this.state.list;
-		//tipo card
 		return (
 			<div className="tienda">
 				<div className="card">
-					<div>
-						<div className="card-content">
-							<div className="card-title">Carrito de Compras</div>
-							<div className="row" content="">
-								<hr />
-								<div className="lista">
-									<div className="col s6">
-										<div>
-											<ul className="collection">
-												{list.map(item =>
-													<li className="collection-item avatar">
-														<img className="circle" src={"/" + item.imagen} alt={item.nombre} />
-														<div className="title"><b>{item.nombre}</b></div>
-														<p>
-															<i>Unidades: </i>{item.cant}
-															<br />
-															<i>Subtotal: </i>${item.total}
-														</p>
-													</li>
-												)}
-											</ul>
-										</div>
+					<div className="card-content">
+						<div className="card-title">Carrito de Compras</div>
+						<div className="row" content="">
+							<hr />
+							<div className="lista">
+								<div className="col s6">
+									<div>
+										<ul className="collection">
+											{list.map(item =>
+												<li className="collection-item avatar">
+													<img className="circle" src={"/" + item.imagen} alt={item.nombre} />
+													<div className="title"><b>{item.nombre}</b></div>
+													<p>
+														<i>Unidades: </i>{item.cant}
+														<br />
+														<i>Subtotal: </i>${item.total}
+													</p>
+												</li>
+											)}
+										</ul>
 									</div>
-									<div className="col s6">
-										<h1>
-											Total: <i> $ {total}</i>
-										</h1>
-										<Link className="btn waves-effect red" to="/vitrina">Cancelar</Link>
-										<a className="btn waves-effect">Pagar</a>
-									</div>
+								</div>
+								<div className="col s6">
+									<h1>
+										Total: <i> $ {total}</i>
+									</h1>
+									<Link className="btn waves-effect red" to="/vitrina">Cancelar</Link>
+									<a className="btn waves-effect">Pagar</a>
 								</div>
 							</div>
 						</div>
